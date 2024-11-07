b2fefa7595ae801519a2da3dfab1de301a05dd97
Abate <147659641+abate-Agegnehu@users.noreply.github.com>
2024-11-07 23:27:46 +0300

    update all

---
+++ src/features/song/MyPlayList.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchSongs,
+  deleteSong,
+  selectAllSongs,
+  getSongStatus,
+  getSongError,
+} from "./songSlice";
+import { Link, useNavigate } from "react-router-dom";
+
+const MyPlayList = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const songs = useSelector(selectAllSongs);
+  const songStatus = useSelector(getSongStatus);
+  const songError = useSelector(getSongError);
+
+  const [playingIndex, setPlayingIndex] = useState(null);
+  const videoRefs = useRef([]);
+
+  useEffect(() => {
+    const storedEmail = sessionStorage.getItem("userEmail");
+    if (storedEmail) {
+      setEmail(storedEmail);
+      console.log(email);
+    }
+    if (songStatus === "idle") {
+      dispatch(fetchSongs());
+    }
+  }, [songStatus, dispatch, email]);
+
+  const handleVideoClick = (index) => {
+    if (playingIndex !== null && playingIndex !== index) {
+      videoRefs.current[playingIndex].pause();
+    }
+    setPlayingIndex(index);
+  };
+
+  const handleVideoDelete = (songId) => {
+    console.log(songId);
+    dispatch(deleteSong({ songId }));
+    navigate("/");
+  };
+
+  let content;
+
+  if (songStatus === "loading") {
+    content = <p>Loading...</p>;
+  } else if (songStatus === "succeeded") {
+    content = (
+      <ul>
+        {songs.map(
+          (song, index) =>
+            song.email === email && (
+              <li key={song._id || index}>
+                <h3>{song.title}</h3>
+                <p>Artist: {song.artist}</p>
+                <video
+                  ref={(el) => (videoRefs.current[index] = el)}
+                  width="320"
+                  height="240"
+                  controls
+                  onPlay={() => handleVideoClick(index)}
+                >
+                  <source src={song.avatar} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+                <button onClick={() => handleVideoDelete(song._id)}>
+                  Delete
+                </button>
+                <Link to={`/editsong/${song._id}`}>Update</Link>
+              </li>
+            )
+        )}
+      </ul>
+    );
+  } else if (songStatus === "failed") {
+    content = <p>Error: {songError}</p>;
+  }
+
+  return (
+    <div>
+      <h2>Song List</h2>
+      {content}
+    </div>
+  );
+};
+
+export default MyPlayList;
---
+++ src/features/song/songSagas.js
@@ -0,0 +1,84 @@
+import { call, put, takeLatest } from "redux-saga/effects";
+import axios from "axios";
+import {
+  fetchSongsSuccess,
+  fetchSongsFailure,
+  addNewSongSuccess,
+  addNewSongFailure,
+  deleteSongSuccess,
+  deleteSongFailure,
+  updateSongSuccess,
+  updateSongFailure,
+} from "./songSlice";
+
+// const SONG_URL = "https://backendmusicplayer.vercel.app/music";
+const SONG_URL = "http://localhost:8888/music";
+
+
+function* fetchSongsSaga() {
+  try {
+    const response = yield call(axios.get, SONG_URL);
+    yield put(fetchSongsSuccess(response.data));
+  } catch (error) {
+    yield put(fetchSongsFailure(error.message));
+  } 
+}
+
+function* addNewSongSaga(action) {
+  const { title, artist,email, video } = action.payload;
+  try {
+    const formData = new FormData();
+    formData.append("video", video);
+    formData.append("title", title);
+    formData.append("email", email);
+    formData.append("artist", artist);
+
+    const response = yield call(axios.post, SONG_URL, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    yield put(addNewSongSuccess(response.data));
+  } catch (error) {
+    yield put(addNewSongFailure(error.message));
+  }
+}
+
+function* deleteSongSaga(action) {
+  try {
+    const { songId } = action.payload;
+    yield call(axios.delete, `${SONG_URL}/${songId}`);
+    yield put(deleteSongSuccess(songId));
+  } catch (error) {
+    yield put(deleteSongFailure(error.message));
+  }
+}
+
+function* updateSongSaga(action) {
+  const { songId, title, artist,email, video } = action.payload;
+  try {
+    const formData = new FormData();
+    formData.append("video", video);
+    formData.append("title", title);
+    formData.append("email", email);
+    formData.append("artist", artist);
+
+    const response = yield call(axios.put, `${SONG_URL}/${songId}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    yield put(updateSongSuccess(response.data));
+  } catch (error) {
+    yield put(updateSongFailure(error.message));
+  }
+}
+
+export function* watchSongSagas() {
+  yield takeLatest("songs/fetchSongs", fetchSongsSaga);
+  yield takeLatest("songs/addNewSong", addNewSongSaga);
+  yield takeLatest("songs/deleteSong", deleteSongSaga);
+  yield takeLatest("songs/updateSong", updateSongSaga);
+}
